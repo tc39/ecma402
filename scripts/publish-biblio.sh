27bae61d9dee50bdb8bcac46d813487ed8bfed23
Kevin Gibbons <bakkot@gmail.com>
2024-02-28 08:07:39 -0800

    Fix publish-biblio.sh to match actual build

--- scripts/publish-biblio.sh
+++ scripts/publish-biblio.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 
-npx ecmarkup --verbose spec.html --write-biblio biblio/biblio.json /dev/null
+npx ecmarkup --verbose --load-biblio @tc39/ecma262-biblio spec/index.html --write-biblio biblio/biblio.json /dev/null
 
 cp LICENSE.md biblio/
 
